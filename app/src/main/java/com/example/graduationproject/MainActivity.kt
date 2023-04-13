db68cdc7532aa79db82c29cdc0f340d746ebed21
magdy <momagdy010299@gmail.com>
2023-04-13 22:01:30 +0200

    Fix Chat Erorr

--- app/src/main/java/com/example/graduationproject/MainActivity.kt
+++ app/src/main/java/com/example/graduationproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.graduationproject
 
 import android.content.Context
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
         super.attachBaseContext(context)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         super.onCreate(savedInstanceState)
         setContent {
             MyApp()
